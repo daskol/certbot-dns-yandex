7272da44269fbdbff0787e5b054f453f2666dd62
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2021-01-02 19:07:33 +0300

    Add packaging files

---
+++ setup.py
@@ -0,0 +1,9 @@
+#   encoding: utf8
+#   filename: setup.py
+#
+#   NOTE This file is used only for editable installations with
+#   `pip install -e` command.
+
+if __name__ == '__main__':
+    from setuptools import setup
+    setup()
