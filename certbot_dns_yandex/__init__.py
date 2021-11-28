2c24b63da7a828c1b15bb8d594881b06e736005b
Daniel Bershatsky <daniel.bershatsky@gmail.com>
2021-11-28 20:02:52 +0300

    Patch built-in str type to support Python 3.8

--- certbot_dns_yandex/__init__.py
+++ certbot_dns_yandex/__init__.py
@@ -1,2 +1,16 @@
 #   encoding: utf8
 #   filename: __init__.py
+
+import builtins
+import sys
+
+if (3, 0) <= sys.version_info < (3, 9):
+
+    class PatchedBuiltInStr(str):
+
+        def removesuffix(self, suffix, /):
+            if self.endswith(suffix):
+                return suffix[:-len(suffix)]
+            return suffix
+
+    builtins.str = PatchedBuiltInStr
